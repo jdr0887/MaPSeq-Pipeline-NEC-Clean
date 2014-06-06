b7a2e1d5185e4faa045562bf76d37aa2cf259d7f
Jason Reilly <jdr0887@renci.org>
2014-06-06 15:31:06 -0400

    workflow api changes

---
+++ nec-clean-messaging/src/main/java/edu/unc/mapseq/messaging/nec/clean/NECCleanMessageService.java
@@ -0,0 +1,75 @@
+package edu.unc.mapseq.messaging.nec.clean;
+
+import javax.jms.Connection;
+import javax.jms.ConnectionFactory;
+import javax.jms.Destination;
+import javax.jms.MessageConsumer;
+import javax.jms.Session;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class NECCleanMessageService {
+
+    private final Logger logger = LoggerFactory.getLogger(NECCleanMessageService.class);
+
+    private Connection connection;
+
+    private Session session;
+
+    private ConnectionFactory connectionFactory;
+
+    private NECCleanMessageListener messageListener;
+
+    private String destinationName;
+
+    public NECCleanMessageService() {
+        super();
+    }
+
+    public void start() throws Exception {
+        logger.info("ENTERING start()");
+        this.connection = connectionFactory.createConnection();
+        this.session = this.connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+        Destination destination = this.session.createQueue(this.destinationName);
+        MessageConsumer consumer = this.session.createConsumer(destination);
+        consumer.setMessageListener(getMessageListener());
+        this.connection.start();
+    }
+
+    public void stop() throws Exception {
+        logger.info("ENTERING stop()");
+        if (this.session != null) {
+            this.session.close();
+        }
+        if (this.connection != null) {
+            this.connection.stop();
+            this.connection.close();
+        }
+    }
+
+    public ConnectionFactory getConnectionFactory() {
+        return connectionFactory;
+    }
+
+    public void setConnectionFactory(ConnectionFactory connectionFactory) {
+        this.connectionFactory = connectionFactory;
+    }
+
+    public NECCleanMessageListener getMessageListener() {
+        return messageListener;
+    }
+
+    public void setMessageListener(NECCleanMessageListener messageListener) {
+        this.messageListener = messageListener;
+    }
+
+    public String getDestinationName() {
+        return destinationName;
+    }
+
+    public void setDestinationName(String destinationName) {
+        this.destinationName = destinationName;
+    }
+
+}
