e1882a58f0f08fe871149bea83c8354a30014558
Jason Reilly <jdr0887@renci.org>
2014-06-12 13:27:57 -0400

    somehow was doing symlink instead of remove

--- nec-clean-workflow/src/main/java/edu/unc/mapseq/workflow/nec/clean/NECCleanWorkflow.java
+++ nec-clean-workflow/src/main/java/edu/unc/mapseq/workflow/nec/clean/NECCleanWorkflow.java
@@ -62,13 +62,6 @@ public class NECCleanWorkflow extends AbstractWorkflow {
         logger.info("htsfSampleSet.size(): {}", htsfSampleSet.size());
 
         String siteName = getWorkflowBeanService().getAttributes().get("siteName");
-        String saHome = getWorkflowBeanService().getAttributes().get("sequenceAnalysisHome");
-
-        File cohortDirectory = new File(saHome, "NEC");
-
-        logger.info("cohortDirectory.getAbsolutePath(): {}", cohortDirectory.getAbsolutePath());
-
-        WorkflowRun workflowRun = getWorkflowPlan().getWorkflowRun();
 
         for (HTSFSample htsfSample : htsfSampleSet) {
 
@@ -78,94 +71,61 @@ public class NECCleanWorkflow extends AbstractWorkflow {
 
             logger.info("htsfSample: {}", htsfSample.toString());
 
-            String subjectName = null;
-            boolean qcPass = false;
-
-            Set<EntityAttribute> attributeSet = workflowRun.getAttributes();
-            if (attributeSet != null && !attributeSet.isEmpty()) {
-                Iterator<EntityAttribute> attributeIter = attributeSet.iterator();
-                while (attributeIter.hasNext()) {
-                    EntityAttribute attribute = attributeIter.next();
-                    String name = attribute.getName();
-                    String value = attribute.getValue();
-                    if ("subjectName".equals(name)) {
-                        subjectName = value;
-                    }
-                    if ("qcPass".equals(name) && StringUtils.isNotEmpty(value) && "true".equalsIgnoreCase(value)) {
-                        qcPass = true;
-                    }
-                }
-            }
-            if (StringUtils.isEmpty(subjectName)) {
-                throw new WorkflowException("invalid subjectName");
-            }
-
-            SequencerRun sequencerRun = htsfSample.getSequencerRun();
-            File outputDirectory = createOutputDirectory(sequencerRun.getName(), htsfSample,
-                    getName().replace("Clean", ""), getVersion());
-            logger.info("outputDirectory.getAbsolutePath() = {}", outputDirectory.getAbsolutePath());
-
-            List<File> readPairList = WorkflowUtil.getReadPairList(htsfSample.getFileDatas(), sequencerRun.getName(),
-                    htsfSample.getLaneIndex());
-
-            // error check
-            if (readPairList.size() != 2) {
-                logger.warn("readPairList.size(): {}", readPairList.size());
-                throw new WorkflowException("Read pair not found");
-            }
-
-            // fastq file names
-            File r1FastqFile = readPairList.get(0);
-            File r2FastqFile = readPairList.get(1);
-
-            // project directories
-            File subjectParentDirectory = new File(cohortDirectory, "subjects");
-            File subjectDirectory = new File(subjectParentDirectory, subjectName);
-
-            File sequenceProjDirectory = new File(subjectDirectory, "sequence");
-            if (!sequenceProjDirectory.exists()) {
-                sequenceProjDirectory.mkdirs();
-            }
-
-            File alignmentProjDirectory = new File(subjectDirectory, "alignment");
-            File alignmentStatProjDirectory = new File(alignmentProjDirectory, "stats");
-            if (!alignmentStatProjDirectory.exists()) {
-                alignmentStatProjDirectory.mkdirs();
-            }
-
-            File qcFailProjDirectory = new File(subjectDirectory, "qc_fail");
-            if (!qcFailProjDirectory.exists()) {
-                qcFailProjDirectory.mkdirs();
-            }
-
-            File idchkProjDirectory = new File(cohortDirectory, "idchk");
-
-            File idchkFVCFProjDirectory = new File(idchkProjDirectory, "fvcf");
-            if (!idchkFVCFProjDirectory.exists()) {
-                idchkFVCFProjDirectory.mkdirs();
-            }
-
-            File idchkECProjDirectory = new File(idchkProjDirectory, "ec");
-            if (!idchkECProjDirectory.exists()) {
-                idchkECProjDirectory.mkdirs();
-            }
-
             try {
 
+                // Files to delete
                 List<File> deleteFileList = new ArrayList<File>();
 
-                File sequencerRunOutputDirectory = new File(getOutputDirectory(), sequencerRun.getName());
-                File analysisWorkflowDirectory = new File(sequencerRunOutputDirectory, "NEC/" + htsfSample.getName());
+                // Get data associated with this htsf sample, start with sequencer run
+                SequencerRun sequencerRun = htsfSample.getSequencerRun();
+                logger.debug("sequencerRun: {}", sequencerRun.toString());
 
+                // get fastq files
+                List<File> readPairList = WorkflowUtil.getReadPairList(htsfSample.getFileDatas(),
+                        sequencerRun.getName(), htsfSample.getLaneIndex());
+                logger.debug("readPairList.size(): {}", readPairList.size());
+
+                // error check
+                if (readPairList.size() != 2) {
+                    logger.warn("readPairList.size(): {}", readPairList.size());
+                    throw new WorkflowException("Read pair not found");
+                }
+
+                // fastq file names
+                // File r1FastqFile = readPairList.get(0);
+                // File r2FastqFile = readPairList.get(1);
+
+                // directories
+                File sequencerRunOutputDirectory = new File(getOutputDirectory(), sequencerRun.getName());
+                // File casavaWorkflowDirectory = new File(sequencerRunOutputDirectory, "CASAVA/" +
+                // htsfSample.getName());
+                //
+                // // cycle through all files in the casavaWorkflowDirectory
+                // for (File f : casavaWorkflowDirectory.listFiles()) {
+                // if (!f.getAbsolutePath().equals(r1FastqFile.getAbsolutePath())
+                // && !f.getAbsolutePath().equals(r2FastqFile.getAbsolutePath())) {
+                // deleteFileList.add(f);
+                // }
+                // }
+
+                File projectDirectory = new File(sequencerRunOutputDirectory, "NEC");
+
+                File analysisWorkflowDirectory = new File(projectDirectory, htsfSample.getName());
+
+                // find analysis files to delete
                 String laneStr = String.format("L%03d", htsfSample.getLaneIndex());
 
+                // cycle through all files in the analysisWorkflowDirectory
                 for (File f : analysisWorkflowDirectory.listFiles()) {
                     String fname = f.getName();
+
+                    // continue only if file name contains correct lane index
                     if (!fname.contains(laneStr)) {
                         deleteFileList.add(f);
                         continue;
                     }
 
+                    // skip files to save / link
                     if (fname.endsWith("fixed-rg.bam") || fname.endsWith("fixed-rg.bai") || fname.endsWith("flagstat")
                             || fname.endsWith("fastqc.zip") || fname.endsWith("fvcf") || fname.endsWith("ec.tsv")
                             || fname.contains(".coverage.")) {
@@ -183,77 +143,6 @@ public class NECCleanWorkflow extends AbstractWorkflow {
                 CondorJob removeJob = builder.build();
                 logger.info(removeJob.toString());
                 graph.addVertex(removeJob);
-
-                // don't link all files, just the analysis directory
-                if (!qcPass) {
-
-                    builder = WorkflowJobFactory.createJob(++count, SymlinkCLI.class, getWorkflowPlan(), htsfSample)
-                            .siteName(siteName);
-                    builder.addArgument(SymlinkCLI.TARGET, outputDirectory.getAbsolutePath())
-                            .addArgument(SymlinkCLI.LINK, qcFailProjDirectory.getAbsolutePath())
-                            .addArgument(SymlinkCLI.SLEEPDURATION, "3");
-                    CondorJob job = builder.build();
-                    logger.info(job.toString());
-                    graph.addVertex(job);
-                    continue;
-                }
-
-                String format = "%s,%s";
-                List<String> targetLinkPairList = new ArrayList<String>();
-                File fastqR1Symlink = new File(sequenceProjDirectory, r1FastqFile.getName());
-                targetLinkPairList.add(String.format(format, r1FastqFile.getAbsolutePath(),
-                        fastqR1Symlink.getAbsolutePath()));
-                File fastqR2Symlink = new File(sequenceProjDirectory, r2FastqFile.getName());
-                targetLinkPairList.add(String.format(format, r2FastqFile.getAbsolutePath(),
-                        fastqR2Symlink.getAbsolutePath()));
-
-                // cycle through all files in the analysisWorkflowDirectory
-                for (File f : outputDirectory.listFiles()) {
-                    String fname = f.getName();
-
-                    if (fname.endsWith("fastqc.zip")) {
-
-                        File targetFile = new File(sequenceProjDirectory, fname);
-                        targetLinkPairList
-                                .add(String.format(format, f.getAbsolutePath(), targetFile.getAbsolutePath()));
-
-                    } else if (fname.endsWith("fixed-rg.bam") || fname.endsWith("fixed-rg.bai")) {
-
-                        File targetFile = new File(alignmentProjDirectory, fname);
-                        targetLinkPairList
-                                .add(String.format(format, f.getAbsolutePath(), targetFile.getAbsolutePath()));
-
-                    } else if (fname.contains(".coverage.") || fname.endsWith("flagstat")) {
-
-                        File targetFile = new File(alignmentStatProjDirectory, fname);
-                        targetLinkPairList
-                                .add(String.format(format, f.getAbsolutePath(), targetFile.getAbsolutePath()));
-
-                    } else if (fname.endsWith("fvcf")) {
-
-                        File targetFile = new File(idchkFVCFProjDirectory, fname);
-                        targetLinkPairList
-                                .add(String.format(format, f.getAbsolutePath(), targetFile.getAbsolutePath()));
-
-                    } else if (fname.endsWith("ec.tsv")) {
-
-                        File targetFile = new File(idchkECProjDirectory, fname);
-                        targetLinkPairList
-                                .add(String.format(format, f.getAbsolutePath(), targetFile.getAbsolutePath()));
-
-                    }
-
-                }
-
-                // new job
-                builder = WorkflowJobFactory.createJob(++count, BatchSymlinkCLI.class, getWorkflowPlan(), htsfSample)
-                        .siteName(siteName);
-                for (String targetLinkPair : targetLinkPairList) {
-                    builder.addArgument(BatchSymlinkCLI.TARGETLINKPAIR, targetLinkPair);
-                }
-                CondorJob symlinkJob = builder.build();
-                logger.info(symlinkJob.toString());
-                graph.addVertex(symlinkJob);
 
             } catch (Exception e) {
                 throw new WorkflowException(e);
